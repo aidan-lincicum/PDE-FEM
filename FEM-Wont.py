bbf1181ce3fa3d85207bb4795efcfd5fe1812e7e
Aidan Lincicum <aidanlincicum@gmail.com>
2024-12-05 11:22:02 -0600

    Create diffusion cat movie

--- FEM-Wont.py
+++ FEM-Wont.py
@@ -5,6 +5,15 @@ from scipy.sparse import coo_matrix
 from scipy.spatial import Delaunay, delaunay_plot_2d
 from numpy.linalg import norm, inv
 from scipy.sparse.linalg import spsolve
+from PIL import Image, ImageOps
+
+#Constants
+num_points = 500
+xmin = -1.5
+xmax = 1.5
+ymin = -1.5
+ymax = 0
+
 
 #Create region
 fig,ax = plt.subplots()
@@ -17,18 +26,18 @@ ax.plot([x0,-x0],[y0,y0],'ro')
 ax.set_aspect("equal")
 
 # upper edge
-x1 = linspace(-x0,x0,20)
+x1 = linspace(-x0,x0,int(num_points/2))
 y1 = -0.6+0.25*x1**2
 
 # lower edge
-th = linspace(pi,2*pi,23)
+th = linspace(pi,2*pi,int(num_points/2 + 2))
 x2 = 1.5*cos(th)
 y2 = 1.5*sin(th)
 x2 = x2[y2<y0]
 y2 = y2[y2<y0]
 
 # grid in interior
-xx,yy = meshgrid(linspace(-1.5,1.5,21),linspace(-2.05,0.95,21))
+xx,yy = meshgrid(linspace(-1.5,1.5,int(num_points/2)),linspace(-2.05,0.95,int(num_points/2)))
 xx[::2,:] += 0.5*3/20
 keep = logical_and(xx**2+yy**2<1.43**2, yy<-0.66+0.25*xx**2)
 
@@ -44,12 +53,12 @@ centroids = triangles.sum(axis=1)/3
 keep = centroids[:,1] < -0.6+0.25*centroids[:,0]**2
 de.simplices = de.simplices[keep,:]
 triangles = de.points[de.simplices,:]
-fig, ax = plt.subplots()
-delaunay_plot_2d(de,ax=ax)
-ax.set_aspect("equal")
-for j in range(xtri.size):
-    ax.text(xtri[j],ytri[j],j)
-fig.savefig("numberedgrid.png")
+# fig, ax = plt.subplots()
+# delaunay_plot_2d(de,ax=ax)
+# ax.set_aspect("equal")
+# for j in range(xtri.size):
+#     ax.text(xtri[j],ytri[j],j)
+# fig.savefig("numberedgrid.png")
 #Find gradient of the triangles
 def mygrad(a,b,c):
     p = c-b
@@ -78,7 +87,7 @@ A7 = coo_matrix(((grads3 * grads1).sum(axis=-1)*areas1,(j3,j1)),shape=(pts,pts))
 A8 = coo_matrix(((grads3 * grads2).sum(axis=-1)*areas1,(j3,j2)),shape=(pts,pts))
 A9 = coo_matrix(((grads3 * grads3).sum(axis=-1)*areas1,(j3,j3)),shape=(pts,pts))
 K = A1 + A2 + A3 + A4 + A5 + A6 + A7 + A8 + A9
-Knew = K.toarray()[41:,41:]
+Knew = K.toarray()[int(num_points):,int(num_points):]
 
 # TODO: M Matrix
 M = zeros((pts, pts))
@@ -93,39 +102,48 @@ for i in range(0, len(j1)):
     M[j3[i], j2[i]] += areas1[i] / 12
     M[j3[i], j3[i]] += areas1[i] / 6
 
-Mnew = M[41:, 41:]
-
-from scipy.linalg import eig
-o1,o2, = eig(Mnew)
-print(o1)
+Mnew = M[int(num_points):, int(num_points):]
 Minv = inv(Mnew)
 
-def icfunc(x, y):
-    return x + y
+im1 = Image.open("imgs/cat.jpg")
+im2 = ImageOps.grayscale(im1)
 
+numpydata = asarray(im2)
+
+dim = numpydata.shape
 ic = zeros(pts)
 for i in range(pts):
-    ic[i] = icfunc(de.points[i, 0], de.points[i, 1])
-ic = ic[41:]
+    xpos = int(de.points[i,0]/(xmax - xmin)*dim[0] - xmin/(xmax - xmin)*dim[0])
+    ypos = int(de.points[i,1]/(ymin - ymax)*dim[1] - ymin/(ymin - ymax)*dim[1])
+    ic[i] = numpydata[ypos,xpos]
+ic = ic[int(num_points):]
 
 def solver(t, vec):
     return matmul(Minv, matmul(-Knew, vec))
 
-tend = 1
-t = linspace(0, tend, 10)
+tend = 0.05
+t_pts = 300
+t = linspace(0, tend, t_pts)
 y1 = solve_ivp(solver, [0, tend], ic, t_eval = t, atol = 1e-9, rtol = 1e-9)
 assert y1.success
-
-
-c = y1.y[:, 9]
-c = hstack((zeros(41),c))
 
 # Plot
 from matplotlib import tri
 mytri = tri.Triangulation(de.points[:,0],de.points[:,1],de.simplices)
 
-# fig,ax = plt.subplots()
-# ax.tricontourf(mytri, c)
+
+# 
 # cax = plt.axes((max(c), min(c), 0.075, 0.8))
 # plt.colorbar(ax, cax=cax, cmap="summer")
-# plt.savefig("hi.png")
+for i in range(t_pts):
+    fig,ax = plt.subplots()
+    c = y1.y[:,i]
+    c = hstack((zeros(int(num_points)),c))
+    # ax = plt.figure().add_subplot(projection='3d')
+    # ax.plot_trisurf(mytri, c, linewidth=0.2, antialiased=True)
+    ax.tricontourf(mytri, c, levels = linspace(-0.01,256,20))
+    #fig.colorbar(pl)
+
+    # ax.set_zlim(-1.5,1)
+    fig.savefig("imgs/fig"+str(i)+".png")
+    plt.close(fig)
---
+++ create-movie.py
@@ -0,0 +1,14 @@
+import os
+import moviepy.video.io.ImageSequenceClip
+from PIL import Image, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+image_files = []
+
+for img_number in range(300): 
+    image_files.append('imgs/fig' + str(img_number) + '.png') 
+
+fps = 30
+
+clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
+clip.write_videofile('imgs/my_new_video.mp4')
