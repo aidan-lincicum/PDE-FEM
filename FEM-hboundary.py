786d72e98ef3d96eb94f638c3281c40ba027c9a4
Aidan Lincicum <aidanlincicum@gmail.com>
2024-12-10 11:57:54 -0600

    New region

--- FEM-hboundary.py
+++ FEM-hboundary.py
@@ -8,7 +8,7 @@ from scipy.sparse.linalg import spsolve
 from PIL import Image, ImageOps
 
 #Constants
-num_points = 200
+num_points = 268
 xmin = -1.5
 xmax = 1.5
 ymin = -1.5
@@ -17,49 +17,58 @@ ymax = 0
 
 #Create region
 fig,ax = plt.subplots()
-y0 = -2+sqrt(3.85)
-x0 = sqrt(4*y0+2.4)
-x = linspace(-x0,x0,300)
-ax.plot(x,-0.6+0.25*x**2)
-ax.plot(x,-sqrt(1.5**2-x**2))
-ax.plot([x0,-x0],[y0,y0],'ro')
-ax.set_aspect("equal")
-
-# upper edge
-x1 = linspace(-x0,x0,int(num_points/2))
-y1 = -0.6+0.25*x1**2
-
-# lower edge
-th = linspace(pi,2*pi,int(num_points/2 + 2))
-x2 = 1.5*cos(th)
-y2 = 1.5*sin(th)
-x2 = x2[y2<y0]
-y2 = y2[y2<y0]
+x1 = linspace(-3,-0.5,25)
+y1 = full(25, 2)
+x2 = full(17, -0.5)
+y2 = linspace(2, 0.25, 17)
+x3 = linspace(-0.5, 0.5, 10)
+y3 = full(10, 0.25)
+x4 = full(17, 0.5)
+y4 = linspace(0.25,2, 17)
+x5 = linspace(0.5, 3, 25)
+y5 = full(25, 2)
+x6 = full(40, 3)
+y6 = linspace(2,-2,40)
+x7 = linspace(3,0.5, 25)
+y7 = full(25, -2)
+x8 = full(17, 0.5)
+y8 = linspace(-2,-0.25, 17)
+x9 = linspace(0.5,-0.5,10)
+y9 = full(10, -0.25)
+x10 = full(17, -0.5)
+y10 = linspace(-0.25,-2,17)
+x11 = linspace(-0.5,-3, 25)
+y11 = full(25, -2)
+x12 = full(40, -3)
+y12 = linspace(-2,2,40)
 
 # grid in interior
-xx,yy = meshgrid(linspace(-1.5,1.5,int(num_points/2)),linspace(-2.05,0.95,int(num_points/2)))
-xx[::2,:] += 0.5*3/20
-keep = logical_and(xx**2+yy**2<1.43**2, yy<-0.66+0.25*xx**2)
+xx,yy = meshgrid(linspace(-2.95,2.95,60),linspace(-1.95,1.95,40))
 
-x3 = xx[keep].reshape(-1)
-y3 = yy[keep].reshape(-1)
+keep = logical_or(logical_or(logical_and(logical_and(xx>-3,xx<-0.5), logical_and(yy>-2,yy<2)), \
+                             logical_and(logical_and(xx>0.5,xx<3), logical_and(yy>-2,yy<2))), \
+                             logical_and(logical_and(xx>-0.5,xx<0.5), logical_and(yy>-0.25,yy<0.25)))
 
-xtri, ytri = hstack((x1,x2,x3)),hstack((y1,y2,y3))
+x13 = xx[keep].reshape(-1)
+y13 = yy[keep].reshape(-1)
+
+xtri, ytri = hstack((x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13)),hstack((y1,y2,y3,y4,y5,y6,y7,y8,y9,y10,y11,y12,y13))
 
 de = Delaunay(stack((xtri,ytri)).T)
 neighbors = de.neighbors
 triangles = de.points[de.simplices,:]
 centroids = triangles.sum(axis=1)/3
-keep = centroids[:,1] < -0.6+0.25*centroids[:,0]**2
+keep = logical_or(logical_or(logical_and(logical_and(centroids[:,0]>-3,centroids[:,0]<-0.5), logical_and(centroids[:,1]>-2,centroids[:,1]<2)), \
+                             logical_and(logical_and(centroids[:,0]>0.5,centroids[:,0]<3), logical_and(centroids[:,1]>-2,centroids[:,1]<2))), \
+                             logical_and(logical_and(centroids[:,0]>-0.5,centroids[:,0]<0.5), logical_and(centroids[:,1]>-0.25,centroids[:,1]<0.25)))
 de.simplices = de.simplices[keep,:]
 triangles = de.points[de.simplices,:]
-# fig, ax = plt.subplots()
-# delaunay_plot_2d(de,ax=ax)
-# ax.set_aspect("equal")
-# for j in range(xtri.size):
-#     ax.text(xtri[j],ytri[j],j)
-# fig.savefig("numberedgrid.png")
-#Find gradient of the triangles
+fig, ax = plt.subplots()
+delaunay_plot_2d(de,ax=ax)
+ax.set_aspect("equal")
+
+fig.savefig("numberedgrid.png")
+
 def mygrad(a,b,c):
     p = c-b
     q = a-b
@@ -76,7 +85,6 @@ grads3,areas3 = mygrad(triangles[:,2],triangles[:,0],triangles[:,1])
 j1,j2,j3 = de.simplices[:,0], de.simplices[:,1], de.simplices[:,2]
 
 pts = len(de.points)
-# from scipy.sparse import coo_matrix
 A1 = coo_matrix(((grads1 * grads1).sum(axis=-1)*areas1,(j1,j1)),shape=(pts,pts))
 A2 = coo_matrix(((grads1 * grads2).sum(axis=-1)*areas1,(j1,j2)),shape=(pts,pts))
 A3 = coo_matrix(((grads1 * grads3).sum(axis=-1)*areas1,(j1,j3)),shape=(pts,pts))
@@ -89,7 +97,6 @@ A9 = coo_matrix(((grads3 * grads3).sum(axis=-1)*areas1,(j3,j3)),shape=(pts,pts))
 K = A1 + A2 + A3 + A4 + A5 + A6 + A7 + A8 + A9
 Knew = K.toarray()[int(num_points):,int(num_points):]
 
-# TODO: M Matrix
 M = zeros((pts, pts))
 for i in range(0, len(j1)):
     M[j1[i], j1[i]] += areas1[i] / 6
@@ -109,16 +116,6 @@ im1 = Image.open("imgs/cat.jpg")
 im2 = ImageOps.grayscale(im1)
 
 numpydata = asarray(im2)
-
-dim = numpydata.shape
-# ic = zeros(pts)
-# for i in range(pts):
-#     xpos = int(de.points[i,0]/(xmax - xmin)*dim[0] - xmin/(xmax - xmin)*dim[0])
-#     ypos = int(de.points[i,1]/(ymin - ymax)*dim[1] - ymin/(ymin - ymax)*dim[1])
-#     ic[i] = numpydata[ypos,xpos]
-# ic = ic[int(num_points):]
-# ic = concatenate((ic, zeros(len(ic))))
-
 def icfunc(x, y):
     return x + y
 
@@ -149,19 +146,11 @@ assert y1.success
 from matplotlib import tri
 mytri = tri.Triangulation(de.points[:,0],de.points[:,1],de.simplices)
 
-
-# 
-# cax = plt.axes((max(c), min(c), 0.075, 0.8))
-# plt.colorbar(ax, cax=cax, cmap="summer")
 for i in range(t_pts):
     fig,ax = plt.subplots()
     c = y1.y[0:int(y1.y.shape[0]/2),i]
     c = hstack((zeros(int(num_points)),c))
-    # ax = plt.figure().add_subplot(projection='3d')
-    # ax.plot_trisurf(mytri, c, linewidth=0.2, antialiased=True)
     ax.tricontourf(mytri, c, levels = linspace(-3,3,20))
-    #fig.colorbar(pl)
 
-    # ax.set_zlim(-1.5,1)
     fig.savefig("imgs/fig"+str(i)+".png")
     plt.close(fig)
--- create-movie.py
+++ create-movie.py
@@ -5,10 +5,10 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 image_files = []
 
-for img_number in range(300): 
+for img_number in range(600): 
     image_files.append('imgs/fig' + str(img_number) + '.png') 
 
 fps = 30
 
 clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
-clip.write_videofile('imgs/my_new_video.mp4')
+clip.write_videofile('imgs/my_new_video.mp4')  
